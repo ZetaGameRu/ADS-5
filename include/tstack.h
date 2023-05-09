1608f0fce27a39b19089e945761da08fcb1002eb
ZetaGameRus <118940070+ZetaGameRu@users.noreply.github.com>
2023-05-09 16:24:27 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,35 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
+#include <iostream>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T *arr;
+    int tIndex;
+
+ public:
+    TStack() {
+        arr = new T[size];
+        tIndex = -1;
+    }
+    bool isEmpty() {
+        return tIndex == -1;
+    }
+    void push(T value) {
+        arr[++tIndex] = value;
+    }
+    T& pop() {
+        return arr[tIndex--];
+    }
+    bool isFull() const {
+        return tIndex == size;
+    }
+    T& get() const {
+        return arr[tIndex];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
